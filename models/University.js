01142e9996292ba2ce3920760e2beb8fdf91955f
Prasanna Jathan <prasannajathan@gmail.com>
2017-11-20 19:11:40 +0530

    University further fields

--- models/University.js
+++ models/University.js
@@ -31,60 +31,91 @@ University.add({
   brief: { type: Types.Html, wysiwyg: true, height: 150 },
   extended: { type: Types.Html, wysiwyg: true, height: 400 },
   //},
+
+  // http://www.4icu.org/ || https://www.niche.com || sa
+
   address: { type: String },
   website: { type: Types.Url },
+
   acronym: { type: String },
   currency: { type: String },
+  // price: {type: Types.Money, format: '$0,0.00', currency: 'en-gb'},
   founded: { type: String },
   motto: { type: String },
   phone: { type: Types.TextArray },
   fax: { type: Types.TextArray },
   locations: { type: String },
-
+  applicationDeadline: { type: Types.Date },
+  applicationFee: { type: Types.Number },
   // FACTS & BADGES
-  avgTuition: { type: String },
-  acceptanceRate: { type: String },
-
+  avgTuition: { type: String }, // 46,300
+  totalCost: { type: String },
+  acceptanceRate: { type: String }, // 25 %
+  studentFacultyRatio: { type: String }, // 4:1
+  RoomAndBoard: { type: String }, // 14,601
+  financialAid: { type: String }, // 35,000 || yes
+  studentClubs: { type: String }, // 650
+  graduateEmployment: { type: String }, // 80%
+  femaleMaleRatio: { type: String }, // 47:53
+  scholarships: { type: String }, // 165,629,375
+  outStudents: { type: String }, // 12.5% - International students
+  //SIZE AND PROFILE
+  students: { type: String }, // 25000 || over-45,000
+  academicCalendar: { type: String }, // Semesters
+  staff: { type: String }, // 2,500-2,999
+  campusSetting: { type: String }, //  Urban
+  controlType: { type: String }, // Private||Private
+  religiousAffiliation: { type: String }, // None
+  entityType: { type: String }, // Non-Profit
+  eveningDegreeProgram: { type: Types.Boolean }, // -:https://www.niche.com/colleges/harvard-university/
   // TUITION
-  BDtuitionLocal: { type: String },
-  BDtuitionInterntl: { type: String },
-  MDtuitionLocal: { type: String },
-  MDtuitionInterntl: { type: String },
-
+  BDtuitionIn: { type: String }, // $ 2,500-5,000  || 2,500-5,000 US$ (1,800-3,700 Euro) - 
+  BDtuitionOut: { type: String }, //   
+  MDtuitionIn: { type: String }, // In-State Tuition
+  MDtuitionOut: { type: String }, // Out-of-State Tuition (International students)
   // PROGRAMS
-  programsOffered: { type: String },
+  totalPrograms: { type: String }, // 48
   BDprograms: { type: Types.Relationship, ref: 'Program', many: true },
   MDprograms: { type: Types.Relationship, ref: 'Program', many: true },
-
   // ADMISSIONS
-  admissionOffice: { type: String },
-  admissionRate: { type: String },
-
-  //SIZE AND PROFILE
-  students: { type: String },
-  academicCalendar: { type: String },
-  staff: { type: String },
-  campusSetting: { type: String },
-  controlType: { type: String },
-  religiousAffiliation: { type: String },
-  entityType: { type: String },
+  admissionOffice: { type: String }, // 51 Dineen Drive Fredericton E3B 5G3+1 (506) 452 0532
+  admissionRate: { type: String }, // 80-90%
+  // value1-value2-value3 (value 1 is minimu & value 2 is averge & value 3 maximum)
+  gmatRange: { type: String }, // value1-value2-value3 || value1-value3
+  greRange: { type: String }, // value1-value2-value3 || value1-value3
+  satRange: { type: String }, // 1400-1600 -:https://www.niche.com/colleges/harvard-university/
+  actRange: { type: String }, // 32-35
+  toeflRange: { type: String }, // value1-value2-value3 || value1-value3
+  ieltsRange: { type: String }, // value1-value2-value3 || value1-value3
+  gpaRange: { type: String }, // value1-value2-value3 || value1-value3
 
   // FACILITIES AND SERVICES
-  library: { type: String },
-  financialAids: { type: String },
+  library: { type: String }, // http://www.4icu.org/reviews/614.htm
   housing: { type: String },
   studyAbroad: { type: String },
   sportFacilities: { type: String },
   distanceLearning: { type: String },
 
+  // RANKINGS
+  timesWorldUniversity: { type: String }, // https://www.timeshighereducation.com/world-university-rankings  
+  forbes: { type: String }, // https://www.forbes.com/top-colleges/list/
+  macleans: { type: String }, // http://www.macleans.ca/education/unirankings/university-rankings-2017/
+  completeUniversityGuide: { type: String }, // https://www.thecompleteuniversityguide.co.uk/
+  financialTimes: { type: String }, // http://rankings.ft.com/businessschoolrankings/global-mba-ranking-2017
+  qs: { type: String }, // https://www.topuniversities.com/university-rankings
+  arwu: { type: String }, // http://www.shanghairanking.com/
+  theEconomist: { type: String }, // http://www.economist.com/whichmba/full-time-mba-ranking
+  usNewsNational: { type: String }, // https://www.usnews.com/best-colleges/rankings/national-universities  
+  usNewsLiberal: { type: String }, // https://www.usnews.com/best-colleges/rankings/national-liberal-arts-colleges
+
   // ACCREDITATION AND RECOGNITION
-  accreditations: { type: Types.TextArray },
+  accreditations: { type: Types.TextArray }, // Foundation for Interior Design Education and Research (FIDER)
 
   // AFFILIATIONS AND MEMBERSHIPS
-  affiliations: { type: Types.TextArray },
+  affiliations: { type: Types.TextArray }, // Canadian Bureau for International Education (CBIE)
 
   // SOCIAL MEDIA
-  facebook: { type: Types.Url },
+  facebook: { type: Types.Url }, // FB page url
   twitter: { type: Types.Url },
   linkedin: { type: Types.Url },
   instagram: { type: Types.Url },
@@ -97,13 +128,7 @@ University.add({
   // WIKIPEDIA ARTICLE
   wikipedia: { type: Types.Url },
 
-
-  // price: {type: Types.Money, format: '$0,0.00', currency: 'en-gb'},
   // location: { type: Types.Location, defaults: { country: 'Australia' } },
-  // introduction: {type: Types.Html, wysiwyg: true, height: 300},
-  // description: {type: Types.Html, wysiwyg: true, height: 300},
-  // manyStrings: { type: Types.TextArray },
-
   city: { type: Types.Relationship, ref: 'UniversityCity' },
   state_province: { type: Types.Relationship, ref: 'UniversityState' },
   country: { type: Types.Relationship, ref: 'UniversityCountry', many: true },
