2fd16b4043b6e6b5f7bd03a325e24fa118b289ce
Prasanna Jathan <prasannajathan@gmail.com>
2018-02-22 20:46:36 +0530

    University program style, search functionality

--- public/js/common.js
+++ public/js/common.js
@@ -17,11 +17,36 @@ font.load().then(function () {
   $("body").addClass("fonts");
 });
 
+// Program search
+$('#searchProgram').on('keyup', function () {
+  var value = this.value;
+  $('#subjects li').hide().each(function () {
+    if ($(this).text().toLowerCase().search(value) > -1) {
+      $(this).show();
+      // $(this).prevAll('.header').first().add(this).show();
+    } else {
+      $(this).hide();
+    }
+  });
+});
+
 // register the service worker if available
 if ('serviceWorker' in navigator) {
+  
   navigator.serviceWorker.register('/sw.js').then(function (reg) {
     //console.log('Successfully registered service worker', reg);
   }).catch(function (err) {
     //console.warn('Error whilst registering service worker', err);
   });
+
+  // unregister service worker
+  // if(window.navigator && navigator.serviceWorker) {
+  //   navigator.serviceWorker.getRegistrations()
+  //   .then(function(registrations) {
+  //     for(let registration of registrations) {
+  //       registration.unregister();
+  //     }
+  //   });
+  // }
+
 }
