60f965c80174c5b80c71174608d6297d6ef64b03
Prasanna Jathan <prasannajathan@gmail.com>
2018-03-09 17:21:03 +0530

    bump service worker

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // use a CACHE_NAME for cache versioning
-var CACHE_NAME = '0.0.3';
+var CACHE_NAME = '4.0.3';
 
 // during the install phase you usually want to cache static assets
 self.addEventListener('install', function (e) {
